df40b81dbbee34acff6c47a1289c8527aae8bb8d
Fanatic191 <krisztian.hekker@gmail.com>
2023-06-19 20:19:47 +0200

    Update locales.ts

--- src/core/plugins/athena-plugin-death/shared/locales.ts
+++ src/core/plugins/athena-plugin-death/shared/locales.ts
@@ -1,4 +1,4 @@
 export const LOCALE_DEATH = {
     UNTIL_RESPAWN: 'újraéledésig',
-    PRESS_KEY_TO_RESPAWN: 'Nyomd meg az E betűt az újraéledéshez.',
+    PRESS_KEY_TO_RESPAWN: 'Nyomd meg az E betüt az újraéledéshez.',
 };
